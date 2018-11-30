853f3ee1a6b9be01f53a5072d1191c9eaca08a04
Sabyasachi Mishra <32372252+Sabyasachi17@users.noreply.github.com>
2018-11-30 19:47:26 +0530

    basic maths operation

---
+++ calculator.sol
@@ -0,0 +1,38 @@
+pragma solidity ^0.4.17;
+contract calculator
+{
+
+    int sum;
+    int prod;
+    int diff;
+    int pow=1;
+
+    
+   function calculator(int a,int b) public
+    {
+         sum=a+b;
+         diff=a-b;
+         prod=a*b;
+         for(int i=1;i<=b;i++)
+         {
+            pow=pow*a;
+         }
+   }
+    function getsum() public view returns(int)
+    {
+        return sum;
+    }
+    function getdiff() public view returns(int)
+    {
+        return diff;
+    }
+    function getprod() public view returns(int)
+    {
+        return prod;
+    }
+    function getpow() public view returns(int)
+    {
+        return pow;
+    }
+    
+}
